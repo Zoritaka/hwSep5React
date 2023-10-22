d59424254d4a624aff6609058424bf1dd7338d06
Luca <Dikhanbaev.2018@mail.ru>
2023-10-22 22:17:00 +0600

    step 2 commit

--- src/pages/MagicBall8.js
+++ src/pages/MagicBall8.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style/magic.css';
-
 const MagicBall8 = () => {
+    const [answer, setAnswer] = useState('');
+    const lstAnswers = ['Бесспорно', 'Предрешено', 'Никаких сомнений', 'Определённо да', 'Можешь быть уверен в этом', 'Мне кажется — «да»', 'Вероятнее всего', 'Хорошие перспективы', 'Знаки говорят — «да»', 'Да', 'Пока не ясно, попробуй снова', 'Спроси позже', 'Лучше не рассказывать', 'Сейчас нельзя предсказать', 'Сконцентрируйся и спроси опять', 'Даже не думай', 'Мой ответ — «нет»', 'По моим данным — «нет»', 'Перспективы не очень хорошие', 'Весьма сомнительно']
+    function randAnswer() {
+        const min = 0;
+        const max = 19;
+        var rand = Math.round(min + Math.random() * (max - min));
+        setAnswer(lstAnswers[rand]);
+    }
+
+    useEffect(() => {
+        
+    }, []);
+    
     function Player(){
         return (
-            <div className="container1">
-
+            <div className='containerM'>
+                <div className="blockM">
+                    <h1>MagicBall8</h1>
+                    <p>Напиши свой вопрос: </p>
+                    <input type='text'></input>
+                    <button onClick={()=>randAnswer()}>Спросить</button>
+                    <p>{answer}</p>
+                </div>
             </div>
         );
     }//Player()
